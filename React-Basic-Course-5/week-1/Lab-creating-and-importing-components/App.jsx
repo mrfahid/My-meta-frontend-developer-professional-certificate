ddb2a8768a6152a13a2a91560d198d8da2a059d7
Shah Fahid <shahfahidbaloch@gmail.com>
2023-12-03 00:25:22 -0800

    importing components

---
+++ React-Basic-Course-5/week-1/Lab-creating-and-importing-components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Heading from './src/Heading'
+
+const App = () => {
+  return (
+    <>
+    <h1>Hello World!</h1>
+    <Heading />
+    </>
+
+  )
+}
+
+export default App
