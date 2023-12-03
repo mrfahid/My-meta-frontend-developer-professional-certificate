8eb25502c7c4c3542a1420d2cb5f10c523a3c62b
Shah Fahid <shahfahidbaloch@gmail.com>
2023-12-02 23:54:36 -0800

    first week ungraded lab asginment

---
+++ React-Basic-Course-5/week-1/Lab-First-Create-Components/src/App.jsx
@@ -0,0 +1,18 @@
+function Heading() {
+  return (
+    <div>
+      <h1>Welcome to the world of Crypto</h1>
+    </div>
+  );
+}
+
+const App = () => {
+  return (
+    <div>
+      This is the starting code for `MY first component` ungraded lab
+      <Heading />
+    </div>
+  );
+};
+
+export default App;
