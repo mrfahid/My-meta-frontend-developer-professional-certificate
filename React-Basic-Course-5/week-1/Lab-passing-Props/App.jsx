f24cde6923668380ee54aad0acf96f38a5267d90
Shah Fahid <shahfahidbaloch@gmail.com>
2023-12-03 00:49:54 -0800

    Lab passing props

---
+++ React-Basic-Course-5/week-1/Lab-passing-Props/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Heading from "./Heading";
+
+const App = () => {
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <p>Welcome to the app.</p>
+      <Heading firstName={"bob"} />
+    </>
+  );
+};
+
+export default App;
---
+++ React-Basic-Course-5/week-1/Lab-passing-Props/Heading.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Heading({ firstName }) {
+  return (
+    <>
+      <h1>Hello {firstName}</h1>
+    </>
+  );
+}
+
+export default Heading;
